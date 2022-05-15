688c0249a9994b486f6f71784bda0cf20473b1c2
prabhu <sekarvprabhu@gmail.com>
2022-05-15 22:12:21 +0530

    Add the following for animations 1.Lottie animation gradle 2.animation json 3.SplashActivity 4.Loading Animations

--- app/src/main/java/com/obvious/nasaapp/ui/MainActivity.kt
+++ app/src/main/java/com/obvious/nasaapp/ui/MainActivity.kt
@@ -1,24 +1,27 @@
-package com.obvious.nasaapp
+package com.obvious.nasaapp.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.obviouc.network.api.ApiHelper
 import com.obviouc.network.api.RetrofitBuilder
 import com.obviouc.network.utils.Status
-import com.obvious.nasaapp.ui.NasaListFragment
+import com.obvious.nasaapp.R
+import com.obvious.nasaapp.databinding.ActivityMainBinding
 import com.obvious.nasaapp.viewmodel.NasaViewModel
 import com.obvious.nasaapp.viewmodel.ViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: NasaViewModel
-
+    private lateinit var _binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
 
         viewModel = ViewModelProvider(
             this,
@@ -28,10 +31,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupObservers() {
-        viewModel.getUsers().observe(this@MainActivity, Observer {
+        viewModel.getUsers().observe(this@MainActivity) {
             it?.let { resource ->
                 when (resource.status) {
                     Status.SUCCESS -> {
+                        _binding.viewAnimation.visibility = View.GONE
+                        _binding.frameLayoutContainer.visibility = View.VISIBLE
                         resource.data.let { nasalist ->
                             supportFragmentManager.beginTransaction().replace(
                                 R.id.frame_layout_container,
@@ -40,17 +45,16 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
                     Status.ERROR -> {
-                        /*recyclerView.visibility = View.VISIBLE
-                        progressBar.visibility = View.GONE*/
-//                        Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                        _binding.viewAnimation.visibility = View.GONE
+                        _binding.viewNoInternetAnimation.visibility = View.VISIBLE
                     }
                     Status.LOADING -> {
-                        /*progressBar.visibility = View.VISIBLE
-                        recyclerView.visibility = View.GONE*/
+                        _binding.viewAnimation.visibility = View.VISIBLE
+                        _binding.frameLayoutContainer.visibility = View.GONE
                     }
                 }
             }
-        })
+        }
     }
 
 
---
+++ app/src/main/java/com/obvious/nasaapp/ui/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.obvious.nasaapp.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.obvious.nasaapp.R
+import java.util.*
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val timerTask = object : TimerTask() {
+            override fun run() {
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+        val timer = Timer()
+        timer.schedule(timerTask, 5000)
+    }
+}
